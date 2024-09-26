f113b2f098ea38a170c3d0a046af3cfee0ab50bd
yamini1611 <yaminipriya1611@gmail.com>
2024-09-26 17:06:56 +0530

    Add project files.

---
+++ BlazorAppTask1/Model/ToDoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorAppTask1.Model
+{
+    public class ToDoItem
+    {
+        public required string Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public bool IsEditing { get; set; }
+    }
+}
